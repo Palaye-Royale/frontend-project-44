de3b850beceea2ae3c21bd2c18398e09f14c5233
Юлия Симсон <juli4a.cat@gmail.com>
2025-02-21 16:31:19 +0300

    task5

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,11 +1,31 @@
 import readlineSync from 'readline-sync';
+ 
+const game = () => {
+    console.log("Welcome to the Brain Games!")
+    const  userName = readlineSync.question('May I have your name? ');
+    console.log(`${'Hello'}, ${userName}!`)
 
-export const isEven = () => {
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-const randomNumber = Math.floor(Math.random() * (100 - 0 + 1) + 0);
+let i = 0;
+
+while (i < 3) {
+const randomNumber = Math.ceil(Math.random() * (100 - 0 + 1) + 0);
 console.log(`${"Question: "}${randomNumber}`);
 
 const userAnswer = readlineSync.question('Your answer: ');
 
-if (randomNumber % 2 !== 0 || )
-};
+const correctAnswer = randomNumber % 2 === 0 ? 'yes' : 'no';
+
+if (userAnswer === correctAnswer) {
+        console.log("Correct!")
+        i +=1;
+} else {
+   console.log(`${"'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again"}, ${userName}!`);
+   i = 0;
+}
+}
+console.log(`${"Congratulations"}, ${userName}!`)
+}
+
+export default game;
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,11 +1,5 @@
 #!/usr/bin/env node
-import {getUserName} from '/home/old_town_road/frontend-project-44/src/cli.js';
-import {isEven} from '/home/old_town_road/frontend-project-44/bin/brain-even.js';
+import getUserName from '/home/old_town_road/frontend-project-44/bin/brain-even.js'
+import game from '/home/old_town_road/frontend-project-44/bin/brain-even.js';
 
-console.log ("Welcome to the Brain Games!");
-
-getUserName();
-
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-isEven();
+game();
