b7986995cba366d3ba3a591e0190d78608b46712
Юлия Симсон <juli4a.cat@gmail.com>
2025-02-21 17:01:12 +0300

    task5

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import getUserName from '/home/old_town_road/frontend-project-44/bin/brain-even.js'
+import getUserName from '/home/old_town_road/frontend-project-44/src/cli.js'
 
 getUserName();
