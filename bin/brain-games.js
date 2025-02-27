f8adad5bdc56f5bc0eba98f6cfd8d80cd62cf28f
Юлия Симсон <juli4a.cat@gmail.com>
2025-02-27 19:18:19 +0300

    var6

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
+import greeting from '/home/old_town_road/frontend-project-44/src/cli.js';
 
 console.log('Welcome to the Brain Games!');
-
-import greeting from '/home/old_town_road/frontend-project-44/src/cli.js';
 
 greeting();
